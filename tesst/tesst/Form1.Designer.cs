19d019e8e305468e64061791da3a039faa76cd11
nguyenvthao710 <81396728+nguyenvthao710@users.noreply.github.com>
2021-05-14 13:27:25 +0700

    quetxevao
    quetxevao

---
+++ tesst/tesst/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace tesst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(557, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Tag = "0";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(100, 103);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(557, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Tag = "0";
+            this.textBox2.Text = "sdas";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(100, 177);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(557, 22);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Tag = "0";
+            this.textBox3.Text = "sdasdsdasdas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 549);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ tesst/tesst/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tesst
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cnn = new SqlConnection(Properties.Settings.Default.KNDL);
+        DataSet QLBaiXe = new DataSet();
+        SqlDataAdapter tb_QuetThe;
+        SqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //DataClasses1DataContext data = new DataClasses1DataContext();
+
+
+        public bool ktKhoaChinhQX(string id, string bienso)
+        {
+            int kq = 0;
+            tb_QuetThe = new SqlDataAdapter("SELECT COUNT(*) FROM QuetXe WHERE IDThe='" + id + "' AND BienSo='" + bienso + "' ", cnn);
+            tb_QuetThe.Fill(QLBaiXe, "TT");
+            DataTable ds = QLBaiXe.Tables["TT"];
+            foreach (DataRow row in ds.Rows)
+            {
+                kq = int.Parse(row[0].ToString());
+            }
+            if(kq==1)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //public bool ktKhoaChinhQX1(string bienso)
+        //{
+        //    int kq = 0;
+        //    tb_QuetThe = new SqlDataAdapter("SELECT COUNT(*) FROM BangThe WHERE MaThe='" + bienso + "'", cnn);
+        //    tb_QuetThe.Fill(QLBaiXe, "TT");
+        //    DataTable ds = QLBaiXe.Tables["TT"];
+        //    foreach (DataRow row in ds.Rows)
+        //    {
+        //        kq = int.Parse(row[0].ToString());
+        //    }
+        //    if (kq == 1)
+        //    {
+        //        return false;
+        //    }
+        //    return true;
+        //}
+
+
+        public bool Them(string id, string bienso, string anhthe)
+        {
+            string sql = "insert into QuetXe values(@IDThe,@BIENSO,@AnhThe)";
+            try
+            {
+                cmd =new   SqlCommand(sql, cnn);
+                cnn.Open();
+                cmd.Parameters.Add("@IDThe", SqlDbType.NVarChar).Value = id;
+                cmd.Parameters.Add("@BIENSO", SqlDbType.NVarChar).Value = bienso;
+                cmd.Parameters.Add("@AnhThe", SqlDbType.NVarChar).Value = anhthe;
+                cmd.ExecuteNonQuery();
+                cnn.Close();
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public string KiemTraTinhtrang(string mathe)
+        {
+            string kq = "";
+            tb_QuetThe = new SqlDataAdapter("SELECT TinhTrang FROM BangThe WHERE MaThe='" + mathe + "'", cnn);
+            tb_QuetThe.Fill(QLBaiXe, "TT");
+            DataTable ds = QLBaiXe.Tables["TT"];
+            foreach (DataRow row in ds.Rows)
+            {
+                kq = row[0].ToString();
+            }
+            return kq;
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            try
+            {
+          
+                
+                if (e.KeyChar == 13)
+                {
+
+                    if (String.IsNullOrEmpty(textBox1.Text) || String.IsNullOrEmpty(textBox2.Text) || String.IsNullOrEmpty(textBox3.Text))
+                    {
+                        MessageBox.Show("Vui lòng nhập mã môn học và tên môn học", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else if (!ktKhoaChinhQX(textBox1.Text,textBox2.Text))
+                    {
+                        MessageBox.Show("Mã này đã tồn tại! Xin vui lòng thử lại", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        //BangThe the = data.BangThes.Where(t => t.MaThe == textBox1.Text).SingleOrDefault();
+                        string kq = KiemTraTinhtrang(textBox1.Text);
+
+                        if ( kq== "False")
+                        {
+                            if(Them(textBox1.Text,textBox2.Text,textBox3.Text))
+                            {
+                                //QuetXe qx = new QuetXe();
+                                //qx.IDThe = textBox1.Text;
+                                //qx.BIENSO = textBox2.Text;
+                                //qx.AnhThe = textBox3.Text;
+                                //data.QuetXes.InsertOnSubmit(qx);
+
+                                //data.SubmitChanges();
+                                MessageBox.Show("Thêm thành công", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            
+                        }
+                        else
+                        {
+                            MessageBox.Show("Tinh trang true");
+                        }
+                        }
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+        }
+    }
+}
